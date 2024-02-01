eb38f18f2321222ed754f2372a9fe4a738cabe15
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-01 22:20:22 +0300

    feat: complete second task

---
+++ lesson_02.js
@@ -0,0 +1,13 @@
+// 1)   1.let counter = {}
+
+//      2.let counter = new Object()
+
+//      3.let counter = Object.create(null)
+
+//      4.function Counter() {}
+//        let counter = new Counter();
+
+//      5.let counter = Object.assign({}, {})
+
+//      6.class Counter {}
+//        let counter = new Counter()
