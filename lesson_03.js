cfc11602b1b2e765ecefc06851f93a9ea54f0161
Yury Kinchin <yury.kinchyn@gmail.com>
2024-02-07 13:58:39 +0300

    feat: complete task_3.1,3.2,3.3

--- lesson_03.js
+++ lesson_03.js
@@ -20,7 +20,7 @@ boundLogger()
 logger.call(obj)
 logger.apply(obj)
 
-// 3)
+// 3.1)
 const arr = [1, 2, 3, 4, 5]
 const sum = arr.reduce((acc, curr) => acc + curr, 0)
 console.log(sum) // 15
@@ -33,3 +33,20 @@ const numbers = [4, 6, 2, 9, 11, 44, 98, 3]
 const max = Math.max(...numbers)
 const min = Math.min(...numbers)
 console.log(max, min) //98 2
+
+// 3.2) Stack - LIFO
+const stack = []
+
+stack.push(1)
+stack.push(2) // add an element to the end of the stack
+
+let lastElement = stack.pop() // remove the last element and return it
+
+// 3.3) Queue - FIFO
+const shopQueue = []
+
+shopQueue.push('Costumer 1')
+shopQueue.push('Costumer 2')
+shopQueue.push('Costumer 3') //  Add a costumer to the end of the queue
+
+let removedElement = shopQueue.shift() // The first customer leaves the queue first
